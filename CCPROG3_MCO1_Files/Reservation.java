f906432fef2a49ca265b9c88a2f9356afb23a74c
Megan Kei Dasal <157576956+megandasal@users.noreply.github.com>
2024-06-10 19:11:11 +0800

    Update Reservation.java

--- CCPROG3_MCO1_Files/Reservation.java
+++ CCPROG3_MCO1_Files/Reservation.java
@@ -1,64 +1,59 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Reservation{
-    private String guestName;
-    private int checkInDate;
-    private int checkOutDate;
-    private int numNights;
+public class Reservation{ 
+  private String guestName;
+  private int checkInDate;
+  private int checkOutDate;
+  private int numNights;
+  private int roomNum;
+  private double roomPrice;
+  private double totalPrice;
 
-    public Reservation(String guestName, int checkInDate, int checkOutDate){
-        this.guestName = guestName;
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-    }
+  public Reservation(String guestName, int checkInDate, int checkOutDate, int roomNum, double price){
+    this.roomNum = roomNum;
+    this.guestName = guestName;
+    this.checkInDate = checkInDate;
+    this.checkOutDate = checkOutDate;
+    this.numNights = checkOutDate - checkInDate; //never used
+    this.roomPrice = price;
+    this.totalPrice = this.roomPrice * (numNights);
+  }
 
-    public void Reserve(String guestName, int checkInDate, int checkOutDate){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name to book reservation: ");
-        guestName = scanner.nextLine();
+  public void printReservationInformation(){
+    System.out.println("Guest Name: " + this.guestName);
+    System.out.println("Room: " + this.roomNum);
+    System.out.println("Check In: " + this.checkInDate);
+    System.out.println("Check Out: " + this.checkOutDate);
+    System.out.println("Cost per night: $" + this.roomPrice);
+    System.out.println("Total Price: $" + this.totalPrice);
 
-        System.out.println("Enter day of reservation: ");
-        checkInDate = scanner.nextInt();
 
+    //view information about selected reservation (guest information, room information, check-in and                  check-out dates, total price of booking and breakdown of cost per night)
+  }
+  
+  public int getRoomNum(){
+    return this.roomNum;
+  }
+  
+  public void calculateNumNights(){
+    // calculate number of nights
+  } 
+  
+  public String getGuestName(){
+    return guestName;
+  }
 
+  public int getCheckInDate(){
+    return checkInDate;
+  } 
 
-    }
+  public int getCheckOutDate(){
+    return checkOutDate;
+  }
 
-    public void calculateNumNights(){
-        // calculate number of nights
-    }
-
-    public String getGuestName(){
-        return guestName;
-    }
-
-    public void setGuestName(String guestName){
-        this.guestName = guestName;
-    }
-
-    public int getCheckInDate(){
-        return checkInDate;
-    }
-
-    public void setCheckInDate(int checkInDate){
-        this.checkInDate = checkInDate;
-    }
-
-    public int getCheckOutDate(){
-        return checkOutDate;
-    }
-
-    public void setCheckOutDate(int checkOutDate){
-        this.checkOutDate = checkOutDate;
-    }
-
-    public int getNumNights(){
-        return numNights;
-    }
-
-    public void setNumNights(int numNights){
-        this.numNights = numNights;
-    }
+  public int getNumNights(){
+    return numNights;
+  }
 
 }
