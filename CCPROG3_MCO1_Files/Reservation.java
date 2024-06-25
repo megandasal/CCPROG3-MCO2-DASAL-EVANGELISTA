63e928d368f54f32ab8abf33710e314ea0d4987d
Megan Kei Dasal <157576956+megandasal@users.noreply.github.com>
2024-06-25 15:57:57 +0800

    Update Reservation.java

--- CCPROG3_MCO1_Files/Reservation.java
+++ CCPROG3_MCO1_Files/Reservation.java
@@ -12,6 +12,14 @@ public class Reservation{
   private double totalPrice;
   private String bookingID;
 
+  /**
+   * Constructs a Reservation object.
+   * @param guestName name of guest who made a reservation
+   * @param checkInDate date a guest checked in
+   * @param checkOutDate date a guest checked out
+   * @param roomNum room number the reservation is made under
+   * @param price the cost of the reservation
+   */
   public Reservation(String guestName, int checkInDate, int checkOutDate, int roomNum, double price){
     this.roomNum = roomNum;
     this.guestName = guestName;
@@ -22,6 +30,9 @@ public class Reservation{
     this.bookingID = this.generateBookingID(roomNum);
   }
 
+  /**
+   * Prints out information about a reservation.
+   */
   public void printReservationInformation(){
     System.out.println("Guest Name: " + this.guestName);
     System.out.println("Booking ID: " + this.bookingID);
@@ -34,39 +45,73 @@ public class Reservation{
 
     //view information about selected reservation (guest information, room information, check-in and                  check-out dates, total price of booking and breakdown of cost per night)
   }
-  
+
+  /**
+   * Retrieves the reservation's room number.
+   * @return room number
+   */
   public int getRoomNum(){
     return this.roomNum;
   }
+
   
   public void calculateNumNights(){
     // calculate number of nights
   } 
-  
+
+  /**
+   * Retrieves the name of the guest who made the reservation.
+   * @return guest name
+   */
   public String getGuestName(){
     return guestName;
   }
 
+  /**
+   * Retrieves the date a guest checked into the hotel.
+   * @return check-in date
+   */
   public int getCheckInDate(){
-    return checkInDate;
-  } 
+      return checkInDate;
+  }
 
+  /**
+   * Retrieves the date a guest checked out of the hotel.
+   * @return check-out date
+   */
   public int getCheckOutDate(){
     return checkOutDate;
   }
 
+  /**
+   * Retrieves the number of nights a guest stayed in the hotel.
+   * @return number of nights
+   */
   public int getNumNights(){
     return numNights;
   }
 
+  /**
+   * Retrieves the total cost of each night a guest stays in a hotel.
+   * @return total cost/price of all nights stayed
+   */
   public double getTotalPrice(){
     return this.totalPrice;
   }
 
+  /**
+   * Retrieves the unique booking ID generated for a reservation.
+   * @return booking ID
+   */
   public String getBookingID() {
     return bookingID;
   }
 
+  /**
+   * Generates a random booking ID for a successful reservation.
+   * @param roomNumber the room number a reservation is made under
+   * @return string of 6 random integers concatenated with the given room number
+   */
   public String generateBookingID(int roomNumber){
     int length = 6; // length of randomly generated integer
     Random random = new Random();
