b368bb45439cbb53efe983b1c3fab3fc9ee93110
Megan Kei Dasal <157576956+megandasal@users.noreply.github.com>
2024-07-17 00:08:36 +0800

    Update ExecutiveRoom.java

--- CCPROG3_MCO1_Files/ExecutiveRoom.java
+++ CCPROG3_MCO1_Files/ExecutiveRoom.java
@@ -2,7 +2,7 @@ public class ExecutiveRoom extends Room {
 
   private static final double EXECUTIVE_MULTIPLIER = 1.35;
 
-  public ExecutiveRoom(int roomCount, double baseRate){
+  public ExecutiveRoom(int roomCount, double baseRate) {
     super(roomCount, baseRate * EXECUTIVE_MULTIPLIER, "Executive");
     this.roomPrice = baseRate * EXECUTIVE_MULTIPLIER;
     this.multiplier = EXECUTIVE_MULTIPLIER;
