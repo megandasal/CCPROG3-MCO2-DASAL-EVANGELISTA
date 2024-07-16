9924232138cb7894ebaec8d694bbdd2e6137e5ad
Megan Kei Dasal <157576956+megandasal@users.noreply.github.com>
2024-07-17 00:08:25 +0800

    Update DeluxeRoom.java

--- CCPROG3_MCO1_Files/DeluxeRoom.java
+++ CCPROG3_MCO1_Files/DeluxeRoom.java
@@ -1,8 +1,8 @@
 public class DeluxeRoom extends Room {
 
   private static final double DELUXE_MULTIPLIER = 1.2;
-  
-  public DeluxeRoom(int roomCount, double baseRate){
+
+  public DeluxeRoom(int roomCount, double baseRate) {
     super(roomCount, baseRate * DELUXE_MULTIPLIER, "Deluxe");
     this.roomPrice = baseRate * DELUXE_MULTIPLIER;
     this.multiplier = DELUXE_MULTIPLIER;
