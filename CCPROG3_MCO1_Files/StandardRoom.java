e677e09ede54197d4d4212c6f6f1d99adafd468c
aiellareigne <aiellareigne20@gmail.com>
2024-07-16 17:00:12 +0800

    Update StandardRoom.java

--- CCPROG3_MCO1_Files/StandardRoom.java
+++ CCPROG3_MCO1_Files/StandardRoom.java
@@ -1,7 +1,11 @@
 public class StandardRoom extends Room {
+
+  private static final double STANDARD_MULTIPLIER = 1.0;
   
   public StandardRoom(int roomCount, double baseRate){
-    super(roomCount, baseRate, "Standard");
+    super(roomCount, baseRate * STANDARD_MULTIPLIER, "Standard");
+    this.roomPrice = baseRate * STANDARD_MULTIPLIER;
+    this.multiplier = STANDARD_MULTIPLIER;
   }
   
 }
